d633178f1cade0a1fc9049a76bc21f16fc3a49e5
Elena Malishenko <e.malishenko@gmail.com>
2017-03-24 18:34:10 +0100

    Added test for checking stickers on main page

---
+++ src/test/java/litecard/shop/MainPageStickers.java
@@ -0,0 +1,46 @@
+package litecard.shop;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+/**
+ * Created by olenamalishenko on 3/24/17.
+ */
+public class MainPageStickers {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void StickerIsAvailable(){
+        driver.get("http://localhost/litecart");
+        wait.until(titleIs("Online Store | My Store"));
+        List<WebElement> items = driver.findElements(By.cssSelector("div.image-wrapper"));
+        for(WebElement item:items){
+            List<WebElement> sticker = item.findElements(By.cssSelector("div.sticker"));
+            Assert.assertEquals("Saticker is absent", 1, sticker.size());
+        }
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
