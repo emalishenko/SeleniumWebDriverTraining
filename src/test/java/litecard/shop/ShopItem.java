8630bb0df229033570752c3c4ebfcd5bb2d55c14
Elena Malishenko <e.malishenko@gmail.com>
2017-03-28 00:33:26 +0200

    Tests for task#10
    Check properties of shopping item on main and details page.

---
+++ src/test/java/litecard/shop/ShopItem.java
@@ -0,0 +1,34 @@
+package litecard.shop;
+
+/**
+ * Created by olenamalishenko on 3/26/17.
+ */
+public class ShopItem {
+    private String name;
+    private String regularPrice;
+    private String campaignPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegularPrice() {
+        return regularPrice;
+    }
+
+    public void setRegularPrice(String regularPrice) {
+        this.regularPrice = regularPrice;
+    }
+
+    public String getCampaignPrice() {
+        return campaignPrice;
+    }
+
+    public void setCampaignPrice(String campaignPrice) {
+        this.campaignPrice = campaignPrice;
+    }
+}
