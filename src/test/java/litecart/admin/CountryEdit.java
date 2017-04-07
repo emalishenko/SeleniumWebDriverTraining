2c22b1cd785dbc56a26a21803bf15acd29de1541
Elena Malishenko <e.malishenko@gmail.com>
2017-04-08 00:49:08 +0200

    Task # 14
    To fix
    Safari, FF (line 24)
    Https vs http

---
+++ src/test/java/litecart/admin/CountryEdit.java
@@ -0,0 +1,56 @@
+package litecart.admin;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+/**
+ * Created by olenamalishenko on 4/7/17.
+ */
+public class CountryEdit extends TestBase {
+
+    @Test
+    public void CountryEditTest(){
+        login();
+        wait.until(titleIs("My Store"));
+        driver.findElement(By.cssSelector("ul#box-apps-menu li#app-:nth-child(3)")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("form[name=countries_form] .row")));
+        driver.findElement(By.cssSelector("form[name=countries_form] .row"))
+                .findElement(By.cssSelector("td:nth-child(5) a")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("td#content i.fa-external-link")));
+        List<WebElement> externalLinks = driver
+                .findElements(By.cssSelector("td#content i.fa-external-link"));
+        //Click through external links
+        String linkUrl, pageUrl;
+        for(WebElement link:externalLinks){
+            String mainWindow = driver.getWindowHandle();
+            link.click();
+            linkUrl = link.findElement(By.xpath("..")).getAttribute("href");
+            System.out.println("Link URL" + linkUrl);
+            String newWindow = getAnotherWindow(mainWindow);
+            driver.switchTo().window(newWindow);
+            pageUrl = driver.getCurrentUrl();
+            System.out.println("Page URL" + pageUrl);
+            //Assert.assertTrue("Incorrect page is opened",linkUrl.equals(pageUrl));
+            driver.close();
+            driver.switchTo().window(mainWindow);
+        }
+
+        //driver.findElement(By.cssSelector("")).click();
+    }
+private String getAnotherWindow(String mainWindow) {
+        wait.until(numberOfWindowsToBe(2));
+        Set<String> allHandlers = driver.getWindowHandles();
+        allHandlers.remove(mainWindow);
+        return allHandlers.iterator().next();
+    }
+
+}
